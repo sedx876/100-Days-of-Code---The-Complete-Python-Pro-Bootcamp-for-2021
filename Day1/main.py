48b69ab35e4d8fbf026dec2e2a6b9b2ecce4422d
sedx876 <swatkins876@gmail.com>
2021-09-03 08:38:05 -0400

    Day1 code

---
+++ Day1/main.py
@@ -0,0 +1,14 @@
+print("Hello World")
+
+
+#Write a program in main.py that prints the same notes from the previous
+# lesson using what you have learnt about the Python print function.
+
+#Warning: The output in your program should match the example output
+# shown below exactly, character for character, even spaces and symbols
+# should be identical, otherwise the tests won't pass.
+
+
+print("Day 1 - Python Print Function")
+print("The Function is declared like this: ")
+print("print('what to print')")
