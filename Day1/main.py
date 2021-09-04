b844a5965a205019704cc2bf30d3fd96cc6577c3
sedx876 <swatkins876@gmail.com>
2021-09-04 09:08:43 -0400

    Day 1 Code

--- Day1/main.py
+++ Day1/main.py
@@ -1,14 +1,18 @@
 print("Hello World")
 
 
-#Write a program in main.py that prints the same notes from the previous
-# lesson using what you have learnt about the Python print function.
-
-#Warning: The output in your program should match the example output
-# shown below exactly, character for character, even spaces and symbols
-# should be identical, otherwise the tests won't pass.
-
-
 print("Day 1 - Python Print Function")
 print("The Function is declared like this: ")
 print("print('what to print')")
+print("Hello World!\nHello World!")
+print("Hello " + "Sharon")
+input("What is your name?")
+print("Hello " + input("What is your name? "))
+
+name = input("What is your name? ")
+print(name)
+
+print("Welcome to the Band Name Generator.")
+street = input("What's name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + street + " " + pet)
