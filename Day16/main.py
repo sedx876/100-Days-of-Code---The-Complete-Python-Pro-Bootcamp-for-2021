5879c2df1feb589df744a4b4abf1488374ef3cfe
sedx876 <swatkins876@gmail.com>
2021-09-06 14:33:39 -0400

    Day16 code

---
+++ Day16/main.py
@@ -0,0 +1,16 @@
+#Object Oriented Programming
+#Class- A blueprint
+#Object- an instance of a class
+
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("green")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
