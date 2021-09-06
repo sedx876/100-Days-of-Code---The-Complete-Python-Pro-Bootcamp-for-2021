fb309741ba1ddf124bb46f930ae0f489d8dbefd7
sedx876 <swatkins876@gmail.com>
2021-09-06 10:33:09 -0400

    Day 4 Code

--- Day4/main.py
+++ Day4/main.py
@@ -6,3 +6,99 @@ print(random_integer)
 random_float = random.random() * 5
 print(random_float)
 
+random_side = random.randint(0, 1)
+if random_side == 1:
+    print("Heads")
+else:
+    print("Tails")
+
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey",
+                     "Georgia", "Connecticut", "Massachusetts",
+                     "Maryland", "South Carolina", "New Hampshire",
+                     "Virginia", "New York", "North Carolina",
+                     "Rhode Island", "Vermont", "Kentucky", "Tennessee",
+                     "Ohio", "Louisiana", "Indiana", "Mississippi",
+                     "Illinois", "Alabama", "Maine", "Missouri",
+                     "Arkansas", "Michigan", "Florida", "Texas", "Iowa",
+                     "Wisconsin", "California", "Minnesota", "Oregon",
+                     "Kansas", "West Virginia", "Nevada", "Nebraska",
+                     "Colorado", "North Dakota", "South Dakota",
+                     "Montana", "Washington", "Idaho", "Wyoming",
+                     "Utah", "Oklahoma", "New Mexico", "Arizona",
+                     "Alaska", "Hawaii"]
+
+print(states_of_america)
+
+dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines",
+               "Apples", "Grapes", "Peaches", "Cherries", "Pears",
+               "Tomatoes", "Celery", "Potatoes"]
+
+names_string = input("Give me everybody's names, seperated by a comma. ")
+names = names_string.split(", ")
+num_items = len(names)
+random_choice = random.randint(0, num_items - 1)
+person_who_will_pay = names[random_choice]
+print(person_who_will_pay + " is going to buy the meal today!")
+
+row1 = ["⬜️", "️⬜️", "️⬜️"]
+row2 = ["⬜️", "⬜️", "️⬜️"]
+row3 = ["⬜️️", "⬜️️", "⬜️️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+
+position = input("Where do you want to put the treasure? ")
+
+horizontal = int(position[0])
+vertical = int(position[1])
+
+map[vertical - 1][horizontal - 1] = "X"
+
+print(f"{row1}\n{row2}\n{row3}")
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+game_images = [rock, paper, scissors]
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+print(game_images[user_choice])
+
+computer_choice = random.randint(0, 2)
+print("Computer chose:")
+print(game_images[computer_choice])
+
+if user_choice >= 3 or user_choice < 0:
+    print("You typed an invalid number, you lose!")
+elif user_choice == 0 and computer_choice == 2:
+    print("You win!")
+elif computer_choice == 0 and user_choice == 2:
+    print("You lose")
+elif computer_choice > user_choice:
+    print("You lose")
+elif user_choice > computer_choice:
+    print("You win!")
+elif computer_choice == user_choice:
+    print("It's a draw")
