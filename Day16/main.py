ce9acd9eedb81714f70baef2023deb84f53a08ca
sedx876 <swatkins876@gmail.com>
2021-09-07 13:26:43 -0400

    Added Day16 Code

--- Day16/main.py
+++ Day16/main.py
@@ -3,14 +3,20 @@
 #Object- an instance of a class
 
 from turtle import Turtle, Screen
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("green")
-timmy.forward(100)
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("green")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+money_machine = MoneyMachine()
+money_machine.report()
+
 
