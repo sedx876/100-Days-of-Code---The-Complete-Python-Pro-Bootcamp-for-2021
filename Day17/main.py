4a03b2a473abc4926e8acc877d1bf1dd5c8fc36b
sedx876 <swatkins876@gmail.com>
2021-09-08 10:36:14 -0400

    Added Day17 Code

--- Day17/main.py
+++ Day17/main.py
@@ -4,7 +4,17 @@ class User:
         self.id = user_id
         self.username = username
         self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
 
 
 user_1 = User("001", "Sharon")
+user_2 = User("002", "Mike")
 print(user_1.username)
+user_1.follow(user_2)
+print(user_1.followers)
+print(user_1.following)
