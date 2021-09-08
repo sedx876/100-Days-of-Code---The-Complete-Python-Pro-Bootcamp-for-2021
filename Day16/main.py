f33994514eb5eb797e85ad4ed9c0f98155caf641
sedx876 <swatkins876@gmail.com>
2021-09-08 09:44:36 -0400

    Day16 code

--- Day16/main.py
+++ Day16/main.py
@@ -3,8 +3,10 @@
 #Object- an instance of a class
 
 from turtle import Turtle, Screen
+from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+menu = Menu()
 
 # timmy = Turtle()
 # print(timmy)
@@ -17,6 +19,23 @@ from money_machine import MoneyMachine
 # my_screen.exitonclick()
 
 money_machine = MoneyMachine()
-money_machine.report()
+coffee_maker = CoffeeMaker()
+is_on= True
+
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        is_enough_ingredients = coffee_maker.is_resource_sufficient(drink)
+        is_payment_successful = money_machine.make_payment(drink.cost)
+        if is_enough_ingredients and is_payment_successful:
+            coffee_maker.make_coffee(drink)
 
 
