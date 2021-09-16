5b98cf8fe2bb7f330340879ce50c9c20b670830e
sedx876 <swatkins876@gmail.com>
2021-09-16 07:16:14 -0400

    AutomateEmail

--- Day24/main.py
+++ Day24/main.py
@@ -3,8 +3,22 @@
 #     print(contents)
 
 
-with open("my_file.txt", mode="a") as file:
-    file.write("\nHere is some new text")
+# with open("my_file.txt", mode="a") as file:
+#     file.write("\nHere is some new text")
+#
+# with open("new_file.txt", mode="w") as file:
+#     file.write("Some more text")
 
-with open("new_file.txt", mode="w") as file:
-    file.write("Some more text")
+PLACEHOLDER = "[name]"
+
+
+with open("./Input/Names/invited_names.txt") as names_file:
+    names = names_file.readlines()
+
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    letter_contents = letter_file.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+            completed_letter.write(new_letter)
